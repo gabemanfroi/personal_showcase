21cb76745875d97c946d807b6577758f8ad7b701
Gabriel Manfroi <32476134+gabemanfroi@users.noreply.github.com>
2021-12-31 23:34:25 -0300

    added service to CMS

--- apps/portfolio/admin.py
+++ apps/portfolio/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Project, Portfolio
+from .models import Project, Portfolio, Service
 from ..shared.admin import BaseEntityModelAdmin, BaseEntityInline
+
+
+@admin.register(Service)
+class ServiceAdmin(BaseEntityModelAdmin):
+    pass
 
 
 class ProjectAdmin(BaseEntityInline):
