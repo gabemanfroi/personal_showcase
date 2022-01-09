23bca9b25053e60a048bb9ded04915956b9d8050
Gabriel Manfroi <32476134+gabemanfroi@users.noreply.github.com>
2022-01-08 22:39:46 -0300

    :rocket: adds new domain

--- personal_showcase/settings.py
+++ personal_showcase/settings.py
@@ -56,6 +56,8 @@ CORS_ALLOWED_ORIGINS = [
     'https://showcase-resume.vercel.app',
     'https://showcaseme.online',
     'http://showcaseme.online'
+    'https://www.showcaseme.online/',
+    'http://www.showcaseme.online/'
 ]
 
 ROOT_URLCONF = 'personal_showcase.urls'
